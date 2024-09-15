6b2a497a9c3ab42672cc09b72a1158feb0204060
arya0509 <151792793+arya0509@users.noreply.github.com>
2024-09-15 09:35:03 -0600

    Adding student.info and altering page.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <header>
+            <h1>Arya Perbhaker</h1>
+            <Link href={"https://github.com/arya0509/cprg306-assignments"}>Github Link</Link>
+        </header>
+    );
+
+    
+};

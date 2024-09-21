76231dc805637f75ac4a7ce5c9b0b104eaf4ba4d
arya0509 <151792793+arya0509@users.noreply.github.com>
2024-09-20 18:14:25 -0600

    Commiting week-3 changes

---
+++ app/week-3/item.js
@@ -0,0 +1,10 @@
+export default function Item(name,quantity,category){
+    return(
+        <ul>
+            <li>{name}</li>
+            <li>{quantity}</li>
+            <li>{category}</li>
+        </ul>
+    );
+    
+}

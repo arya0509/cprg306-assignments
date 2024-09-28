06e888407f692c43407e34bbddf70c67b21ac77d
arya0509 <151792793+arya0509@users.noreply.github.com>
2024-09-27 20:47:26 -0600

    Some UI changes

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -24,9 +24,9 @@ export default function NewItem() {
     };
     return(
         <div className="max-h-screen flex justify-center">
-            <div className="bg-white w-40 h-12 flex flex-row justify-around mt-8">
+            <div className="bg-white w-40 h-12 flex flex-row justify-center mt-8">
                 <p className="text-black flex-1 mt-3 ml-3">{quantity}</p>
-                <div className="flex flex-1 w-10 justify-around">
+                <div className="flex flex-1 w-15 justify-between  mr-2">
                     <button onClick={decrement} className={` text-white w-8 h-6 mt-3 rounded ${quantity===1 ?'bg-gray-500 ':'bg-blue-500  hover:bg-blue-700'}`}>-</button>
                     <button onClick={increment} className={` text-white  w-8 h-6 mt-3 rounded ${quantity===20 ?'bg-gray-500 ':'bg-blue-500  hover:bg-blue-700'}`}>+</button>
                 </div>   

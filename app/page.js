2eb8d23652025dce7e02b0446156b8803fb691a1
arya0509 <151792793+arya0509@users.noreply.github.com>
2024-10-12 17:05:43 -0600

    Adding hover to links

--- app/page.js
+++ app/page.js
@@ -5,15 +5,15 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href={"http://localhost:3000/week-2"}>Week-2 page </Link>
+      <Link className="hover:text-green-600" href={"http://localhost:3000/week-2"}>Week-2 page </Link>
       <br></br>
-      <Link href={"http://localhost:3000/week-3"}>Week-3 page</Link>
+      <Link className="hover:text-green-600" href={"http://localhost:3000/week-3"}>Week-3 page</Link>
       <br></br>
-      <Link href={"http://localhost:3000/week-4"}>Week-4 page</Link>
+      <Link className="hover:text-green-600" href={"http://localhost:3000/week-4"}>Week-4 page</Link>
       <br></br>
-      <Link href={"http://localhost:3000/week-5"}>Week-5 page</Link>
+      <Link className="hover:text-green-600" href={"http://localhost:3000/week-5"}>Week-5 page</Link>
       <br></br>
-      <Link href={"http://localhost:3000/week-6"}>Week-6 page</Link>
+      <Link className="hover:text-green-600" href={"http://localhost:3000/week-6"}>Week-6 page</Link>
    
     </main>
     
